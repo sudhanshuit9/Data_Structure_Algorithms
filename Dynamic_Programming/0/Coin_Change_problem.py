85bcae76eb28367505f5ccc5c93ef66ccd8699df
Sudhanshu Singh <91426269+sudhanshuit9@users.noreply.github.com>
2024-11-08 14:40:41 +0530

    Code added change

---
+++ Dynamic_Programming/0/Coin_Change_problem.py
@@ -0,0 +1,9 @@
+# Given a set of coins with different denominations and an amount, find the minimum number of coins needed to make up that amount. If it is not possible, return -1.
+
+def coin_change(coins, amount):
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+    for coin in coins:
+        for i in range(coin, amount + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[amount] if dp[amount] != float('inf') else -1
---
+++ Dynamic_Programming/0/Levenshtein Distance.py
@@ -0,0 +1,16 @@
+# Given two strings, find the minimum number of operations required to convert one string into the other. Operations include insert, delete, and replace a character.
+
+def edit_distance(A, B):
+    m, n = len(A), len(B)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j  # Inserting all characters of B
+            elif j == 0:
+                dp[i][j] = i  # Deleting all characters of A
+            elif A[i - 1] == B[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+    return dp[m][n]
